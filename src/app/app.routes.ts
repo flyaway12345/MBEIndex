c0f6cd2cd418c31bc693adc99bf73bdfb291e71c
Evan Haggard <evanhaggard123@gmail.com>
2025-01-21 19:19:21 -0800

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { MbeIndexComponent } from './MBE index/mbe-index.component';
-
-export const routes: Routes = [
-  {path:'**', component: MbeIndexComponent}
-];
